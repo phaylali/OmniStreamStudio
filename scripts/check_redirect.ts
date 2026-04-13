4c3be44f869939f954da26349b6fea7647edfa44
Phaylali <admin@omniversify.com>
2026-04-13 03:06:55 +0100

    refactor: clean up vite config and ensure module compatibility in check_redirect script

--- scripts/check_redirect.ts
+++ scripts/check_redirect.ts
@@ -2,3 +2,5 @@ const username = process.argv[2];
 const response = await fetch(`https://www.twitch.tv/${username}`);
 console.log("FINAL URL: " + response.url);
 console.log("STATUS: " + response.status);
+
+export {};
--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import { resolve } from "path";
 
 dotenv.config();
 
-// @ts-expect-error process is a nodejs global
 const host = process.env.TAURI_DEV_HOST;
 
 // https://vite.dev/config/
@@ -16,10 +15,10 @@ export default defineConfig(async () => ({
     host: host || false,
     hmr: host
       ? {
-          protocol: "ws",
-          host,
-          port: 1421,
-        }
+        protocol: "ws",
+        host,
+        port: 1421,
+      }
       : undefined,
     watch: {
       ignored: ["**/src-tauri/**"],
